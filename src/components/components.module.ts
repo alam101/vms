aacea630174d75fc7166f12b919fea000b7d231d
Fakhre Alam <fakhre.alam101@in.ibm.com>
2018-04-10 02:04:10 +0530

    commit with UI modification

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 import { HeaderComponent } from './header/header';
+import { FooterComponent } from './footer/footer';
 
 
 @NgModule({
   imports: [IonicModule],
-  exports: [HeaderComponent],
-  declarations: [HeaderComponent],
+  exports: [HeaderComponent,
+    FooterComponent],
+  declarations: [HeaderComponent,
+    FooterComponent],
   providers: [],
 })
 
---
+++ src/components/footer/footer.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer',
+  templateUrl: 'footer.html'
+})
+export class FooterComponent {
+  constructor() {
+    console.log('Hello FooterComponent Component');
+    
+  }
+
+}
